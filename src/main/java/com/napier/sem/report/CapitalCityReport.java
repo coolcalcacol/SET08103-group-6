841646b8b12d7b463aaae6f06e39a5e764afb7fe
Alex <alex.dumbreck@gmail.com>
2023-02-16 09:29:43 +0000

    Update Report classes to meet spec

--- src/main/java/com/napier/sem/report/CapitalCityReport.java
+++ src/main/java/com/napier/sem/report/CapitalCityReport.java
@@ -3,7 +3,7 @@ package com.napier.sem.report;
 public class CapitalCityReport extends BaseReport {
     public String country;
 
-    public CapitalCityReport(String country, String name, String population) {
+    public CapitalCityReport(String name, String population, String country) {
         super(name, population);
         this.country = country;
     }
