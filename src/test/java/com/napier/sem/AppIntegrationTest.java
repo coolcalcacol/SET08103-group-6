b3f2e0f73e189ffb183657b0e5223085f9041f31
Alex <alex.dumbreck@gmail.com>
2023-02-21 20:22:18 +0000

    jacoco now runs correctly

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class AppIntegrationTests {
+public class AppIntegrationTest {
 
     @Test
     void basicTest() {
--- src/test/java/com/napier/sem/AppUnitTest.java
+++ src/test/java/com/napier/sem/AppUnitTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class AppUnitTests {
+public class AppUnitTest {
 
     @Test
     void basicTest() {
