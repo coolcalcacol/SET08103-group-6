a65905f9e6161258f267979031d6eb9913f1df28
Alex <alex.dumbreck@gmail.com>
2023-02-21 19:24:06 +0000

    add basic test

--- src/test/java/com/napier/sem/AppUnitTests.java
+++ src/test/java/com/napier/sem/AppUnitTests.java
@@ -1,4 +1,13 @@
 package com.napier.sem;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AppUnitTests {
+
+    @Test
+    void basicTest() {
+        assertEquals(1, 1);
+    }
 }
