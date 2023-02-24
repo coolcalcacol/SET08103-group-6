6636d1a6e38b279c687cb364674be7dc7b18e00b
Alex <alex.dumbreck@gmail.com>
2023-02-24 22:35:27 +0000

    initiate DB connection for all Integration tests

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -1,10 +1,19 @@
 package com.napier.sem;
 
+import com.napier.sem.database.DB;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppIntegrationTest {
+    static DB db;
+    @BeforeAll
+    static void init() {
+        db = new DB("localhost", "33060", "world", "root", "group6Pass");
+
+        db.connect();
+    }
 
     @Test
     void basicTest() {
