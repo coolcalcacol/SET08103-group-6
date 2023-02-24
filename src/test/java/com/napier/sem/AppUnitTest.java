923a288f6ea17fb4896908a31a0b3c4526efe9f8
Alex <alex.dumbreck@gmail.com>
2023-02-24 22:35:54 +0000

    Add unit tests for all the Report classes

--- src/test/java/com/napier/sem/AppUnitTest.java
+++ src/test/java/com/napier/sem/AppUnitTest.java
@@ -1,5 +1,8 @@
 package com.napier.sem;
 
+import com.napier.sem.report.*;
+import com.napier.sem.report.extended.ExtendedCapitalCityReport;
+import com.napier.sem.report.extended.ExtendedCityReport;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,5 +12,74 @@ public class AppUnitTest {
     @Test
     void basicTest() {
         assertEquals(1, 1);
+    }
+
+    // Abstract base report
+    @Test
+    void baseReportTest() {
+        BaseReport report = new BaseReport("name", "population") {};
+        assertEquals("name", report.name);
+        assertEquals("population", report.population);
+    }
+
+    // Basic report types
+
+    @Test
+    void capitalCityReportTest() {
+        CapitalCityReport report = new CapitalCityReport("name", "population", "country");
+        assertEquals("name", report.name);
+        assertEquals("population", report.population);
+        assertEquals("country", report.country);
+    }
+
+    @Test
+    void cityReportTest() {
+        CityReport report = new CityReport("name", "population", "district", "country");
+        assertEquals("name", report.name);
+        assertEquals("population", report.population);
+        assertEquals("district", report.district);
+        assertEquals("country", report.country);
+    }
+
+    @Test
+    void countryReportTest() {
+        CountryReport report = new CountryReport("name", "population", "code", "continent", "region", "capital");
+        assertEquals("name", report.name);
+        assertEquals("population", report.population);
+        assertEquals("code", report.code);
+        assertEquals("continent", report.continent);
+        assertEquals("region", report.region);
+        assertEquals("capital", report.capital);
+    }
+
+    @Test
+    void populationReportTest() {
+        PopulationReport report = new PopulationReport("name", "population", "populationInCities", "populationOutCities");
+        assertEquals("name", report.name);
+        assertEquals("population", report.population);
+        assertEquals("populationInCities", report.populationInCities);
+        assertEquals("populationOutCities", report.populationOutCities);
+    }
+
+    // Extended report types
+    @Test
+    void extendedCapitalCityReportTest() {
+        ExtendedCapitalCityReport report = new ExtendedCapitalCityReport("name", "population", "country", "continent", "region");
+        assertEquals("name", report.name);
+        assertEquals("population", report.population);
+        assertEquals("country", report.country);
+        assertEquals("continent", report.continent);
+        assertEquals("region", report.region);
+    }
+
+    @Test
+    void extendedCityReportTest() {
+        ExtendedCityReport report = new ExtendedCityReport("name", "population", "district", "country", "continent", "region");
+        assertEquals("name", report.name);
+        assertEquals("population", report.population);
+        assertEquals("district", report.district);
+        assertEquals("country", report.country);
+        assertEquals("continent", report.continent);
+        assertEquals("region", report.region);
     }
 }
